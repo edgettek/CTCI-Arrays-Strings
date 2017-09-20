c8a4e696af073f288443d618d9467721f883a08d
Kyle Edgette <edgettek@gmail.com>
2017-09-20 14:45:20 -0400

    Implement isUnique() for P1.1

---
+++ P1_1.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+public class P1_1 {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String> words = new ArrayList<String>();
+		
+		words.add("Kyle");
+		words.add("aaa");
+		words.add("Hillary");
+		words.add("Geronimo");
+		words.add("Renato");
+		words.add("aba");
+		words.add("abc");
+		
+		for(String s : words) {
+			System.out.println(s + " is unique? " + isUnique(s));
+		}
+		
+	}
+	
+	/*
+	 * Solution to P1.1 : implement an algorithm to see if a string has all unique characters
+	 * 
+	 * Runtime: O(N) where N is the number of characters in the string
+	 */
+	public static boolean isUnique(String word) {
+		
+		int[] letterCount = new int[26];
+		
+		word = word.toLowerCase();
+		
+		char [] chars = word.toCharArray();
+		
+		for(int i = 0; i < chars.length; i++) {
+			int value = chars[i];
+			value = value-97;
+			
+			if(letterCount[value] > 0) {
+				return false;
+			}
+			else {
+				letterCount[value]++;
+			}
+
+		}
+		
+		return true;
+	}
+
+}
