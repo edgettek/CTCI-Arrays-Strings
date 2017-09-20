589f712bc5752a45994140f79d826f0ec7bd58a3
Kyle Edgette <edgettek@gmail.com>
2017-09-20 15:12:34 -0400

    first attempt at P1.2

--- Chapter_1.java
+++ Chapter_1.java
@@ -4,6 +4,8 @@ public class Chapter_1 {
 	
 	public static void main(String[] args) {
 		
+		// P1.1
+		
 		ArrayList<String> words = new ArrayList<String>();
 		
 		words.add("Kyle");
@@ -15,9 +17,18 @@ public class Chapter_1 {
 		words.add("abc");
 		words.add("Kk");
 		
-		for(String s : words) {
-			System.out.println(s + " is unique? " + isUnique(s));
-		}
+//		for(String s : words) {
+//			System.out.println(s + " is unique? " + isUnique(s));
+//		}
+		
+		// P1.2
+		
+		System.out.println("Are Kyle and elKy permutations? " + isPermutation("Kyle", "elKy"));
+		System.out.println("Are abca and baac permutations? " + isPermutation("abca", "baac"));
+		System.out.println("Are nope and hello permutations? " + isPermutation("nope", "hello"));
+		System.out.println("Are Abc and abc permutations? " + isPermutation("Abc", "abc"));
+		System.out.println("Are GOAT and TAOG permutations? " + isPermutation("GOAT", "TAOG"));
+
 		
 	}
 	
@@ -47,4 +58,39 @@ public class Chapter_1 {
 		return true;
 	}
 
+	/*
+	 * Solution to P1.2 : implement an algorithm to see if one string is a permutation of another
+	 */
+	public static boolean isPermutation(String s_1, String s_2) {
+		
+		//Algorithm: count number of occurrences of each letter in each string, if # occurrences match then permutation!
+		
+		int[] charCount = new int[128];
+		
+		char current;
+		int index = -1;
+		
+		for(int i = 0; i < s_1.length(); i++) {
+			current = s_1.charAt(i);
+			
+			index = current;
+			
+			charCount[index]++;	
+		}
+		
+		for(int i = 0; i < s_2.length(); i++) {
+			current = s_2.charAt(i);
+			
+			index = current;
+			
+			charCount[index]--;	
+		}
+		
+		for(int i = 0; i < charCount.length; i++) {
+			if(charCount[i] != 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
